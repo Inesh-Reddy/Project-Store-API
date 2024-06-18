6ff95e6b6e0a624dfc3f3e30fdb5f69e3a05182b
Inesh-Reddy <ineshreddy007@gmail.com>
2024-06-18 15:11:43 +0530

    adding pagination

--- controllers/products.js
+++ controllers/products.js
@@ -40,6 +40,13 @@ const getAllProducts = async (req, res) => {
         result = result.select(fieldsList);
     }
 
+    const page = Number(req.query.page) || 1;
+    const limit  = Number(req.query.limit) || 5;
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit);
+
+
     const Products = await result;
     
 
